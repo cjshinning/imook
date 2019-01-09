171a703f3718c31e64f070e5fc08c6282743db26
cjshinning <784319417@qq.com>
2019-01-09 22:41:57 +0800

    react基础知识2

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ class App extends React.Component{
     return (
       <div>
         <h2>独立团，团长{boss}</h2>
-        <一营></一营>
+        <一营 老大="张大喵"></一营>
+        <骑兵连 老大="孙德胜"></骑兵连>
       </div>
     )
   }
 }
 
+function 骑兵连(props){
+  return <h2>骑兵连连长，{props.老大}</h2>
+}
+
 class 一营 extends React.Component{
   render(){
-    const boss = '张大喵';
     return (
       <div>
-        <h2>一营营长，{boss}</h2>
+        <h2>一营营长，{this.props.老大}</h2>
       </div>
     )
   }
