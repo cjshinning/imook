33cd5555ecec0f86b006ce196ed7cd9427001d28
cjshinning <784319417@qq.com>
2019-01-09 22:50:25 +0800

    react基础知识3：state

--- src/App.js
+++ src/App.js
@@ -14,14 +14,25 @@ class App extends React.Component{
 }
 
 function 骑兵连(props){
-  return <h2>骑兵连连长，{props.老大}</h2>
+  return <h2>骑兵连连长{props.老大}，冲啊！</h2>
 }
 
 class 一营 extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      solders: ['虎子','柱子','王根生']
+    }
+  }
   render(){
     return (
       <div>
         <h2>一营营长，{this.props.老大}</h2>
+        <ul>
+          {this.state.solders.map(v=>{
+            return <li key={v}>{v}</li>
+          })}
+        </ul>
       </div>
     )
   }
