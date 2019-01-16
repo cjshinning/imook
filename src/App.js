d4d7f0ea190dfeffcdee6b0ccb94b82b6f3feadf
cjshinning <784319417@qq.com>
2019-01-16 22:13:42 +0800

    组件解耦

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {addGun} from './index.redux'
 
 class App extends React.Component{
     constructor(){
@@ -7,10 +6,13 @@ class App extends React.Component{
     }
     render(){
         const num = this.props.store.getState()
+        const addGun = this.props.addGun
+        const removeGun = this.props.removeGun
         return (
             <div>
                 <h2>现在有机枪{num}把</h2>
                 <button onClick={()=>{this.props.store.dispatch(addGun())}}>申请武器</button>
+                <button onClick={()=>{this.props.store.dispatch(removeGun())}}>上交武器</button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
-import {counter} from './index.redux'
+import {counter,addGun,removeGun} from './index.redux'
 import App from './App'
 
 let store=createStore(counter)
 
 function render(){
-    ReactDOM.render(<App store={store}/>, document.getElementById('root'))
+    ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun}/>, document.getElementById('root'))
 }
 
 render()
