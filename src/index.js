648062e555cf8f8cacd267c76bffb634748606e9
cjshinning <784319417@qq.com>
2019-03-13 22:55:51 +0800

    初识react router4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore,applyMiddleware,compose} from 'redux'
+import {BrowserRouter,Route,Link} from 'react-router-dom'
 import {counter,addGun,removeGun,addGunAsync} from './index.redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
@@ -11,9 +12,28 @@ let store=createStore(counter,compose(
     window.devToolsExtension?window.devToolsExtension():f=>f
 ));
 
+function Erying(){
+    return <h2>二营</h2>
+}
+
+function Qibilian(){
+    return <h2>骑兵连</h2>
+}
+
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li><Link to='/'>一营</Link></li>
+                    <li><Link to='/erying'>二营</Link></li>
+                    <li><Link to='/qibilian'>骑兵连</Link></li>
+                </ul>
+                <Route path='/' exact component={App}></Route>
+                <Route path='/erying' component={Erying}></Route>
+                <Route path='/qibilian' component={Qibilian}></Route>
+            </div>
+        </BrowserRouter>
     </Provider>, 
     document.getElementById('root')
 )
