102035681429fa52cdab804aebaef9a9a636d92e
cjshinning <784319417@qq.com>
2019-03-16 21:49:41 +0800

    react router基础使用

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore,applyMiddleware,compose} from 'redux'
-import {BrowserRouter,Route,Link} from 'react-router-dom'
+import {BrowserRouter,Route,Link,Redirect,Switch} from 'react-router-dom'
 import {counter,addGun,removeGun,addGunAsync} from './index.redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
@@ -20,6 +20,13 @@ function Qibilian(){
     return <h2>骑兵连</h2>
 }
 
+class Test extends React.Component{
+    render(){
+        console.log(this.props)
+        return <h2>测试组件{this.props.match.params.location}</h2>
+    }
+}
+
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
@@ -29,11 +36,16 @@ ReactDOM.render(
                     <li><Link to='/erying'>二营</Link></li>
                     <li><Link to='/qibilian'>骑兵连</Link></li>
                 </ul>
-                <Route path='/' exact component={App}></Route>
-                <Route path='/erying' component={Erying}></Route>
-                <Route path='/qibilian' component={Qibilian}></Route>
+                <Switch>
+                    <Route path='/' exact component={App}></Route>
+                    <Route path='/erying' component={Erying}></Route>
+                    <Route path='/qibilian' component={Qibilian}></Route>
+                    <Route path='/:location' component={Test}></Route>
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>, 
     document.getElementById('root')
 )
+
+
