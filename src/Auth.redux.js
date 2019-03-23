052ca8278a1cad841d57c1a9e92be499d1beb3a9
cjshinning <784319417@qq.com>
2019-03-23 15:58:00 +0800

    前后端联调1

--- src/Auth.redux.js
+++ src/Auth.redux.js
@@ -1,16 +1,36 @@
+import axios from 'axios'
+
 const LOGIN = 'LOGIN'
 const LOGOUT = 'LOGOUT'
+const USER_DATA = 'USER_DATA'
 
-export function auth(state={isAuth:false,user:'李云龙'},action){
+export function auth(state={isAuth:false,user:'李云龙',age:0},action){
     switch(action.type){
         case LOGIN:
             return {...state,isAuth:true}
         case LOGOUT:
             return {...state,isAuth:false}
+        case USER_DATA: 
+            return {...state, user: action.payload.user, age: action.payload.age}
         default:
             return state
     }
 } 
+
+export function getUserData(){
+    return dispatch => {
+        axios.get('/data')
+            .then(res => {
+                if(res.status == 200){
+                    dispatch(userData(res.data))
+                }
+            })
+    }
+}
+
+export function userData(data){
+    return {type: USER_DATA, payload: data}
+}
 
 export function login(){
     return {type:LOGIN}
