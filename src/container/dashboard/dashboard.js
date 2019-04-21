29cdf2eaaed9519c6473d880377d5d63448b3133
cjshinning <784319417@qq.com>
2019-04-21 23:22:30 +0800

    修正消息列表

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -6,11 +6,8 @@ import NavLinkBar from '../../component/navlink/navlink'
 import Boss from '../../component/boss/boss'
 import Genius from '../../component/gunius/genius'
 import User from '../../component/user/user'
+import Msg from '../../component/msg/msg'
 import {getMsgList,recvMsg} from '../../redux/chat.redux'
-
-function Msg(){
-    return <h2>消息列表页</h2>
-}
 
 @connect(
     state=>state,
